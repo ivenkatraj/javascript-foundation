85c3d5912b479c9a6a6d1c215207bd52a02c992d
ivenkatraj <venkatraj442@gmail.com>
2024-12-11 18:55:52 +0530

    practice

--- 07-numbers/practice/script.js
+++ 07-numbers/practice/script.js
@@ -4,3 +4,16 @@ const decimalNumber = 0.5;
 const result = wholeNumber + decimalNumber;
 
 console.log(result);
+console.log(typeof result);
+
+//using strings and number
+
+const string = "Hello";
+const number = 5;
+
+const output = string + number;
+
+// The output is Hello5
+// Because of concatenation
+
+console.log(output);
