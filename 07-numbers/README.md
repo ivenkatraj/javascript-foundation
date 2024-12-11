fa4fadeefbe06198c988c42564aaba40722b4c5d
ivenkatraj <venkatraj442@gmail.com>
2024-12-11 18:33:19 +0530

    `Number` topic

---
+++ 07-numbers/README.md
@@ -0,0 +1,8 @@
+# Number
+
+Number is also an data types used to store in identifier names.
+
+Numbers can be stored in whole number or decimal numbers.
+
+Eg: const wholeNumber = 5; <br/>
+const decimalNumber = 0.23;
---
+++ 07-numbers/js/script.js
@@ -0,0 +1,8 @@
+const wholeNumber = 5;
+const decimalNumber = 0.21;
+
+//The number data type can be stored in var, let or const.
+//Need not to be specified in special characters.
+
+console.log(wholeNumber);
+console.log(decimalNumber);
