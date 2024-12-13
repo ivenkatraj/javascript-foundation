cd63f3f025cee18498e4c7065e48c6bf4aa4d0dd
ivenkatraj <venkatraj442@gmail.com>
2024-12-13 13:09:12 +0530

    Objects practice

--- 10-objects/practice/script.js
+++ 10-objects/practice/script.js
@@ -5,10 +5,8 @@ let object ={
     dob: '24-04-2002',
     nickname: [1,2,3]
 }
-
-let array = [1,2,3];
-
-console.log(typeof array);
-
 //using dot notation to call the value of a particular key
 console.log(typeof object.nickname);
+
+let array = [1,2,3];
+console.log(typeof array);
