f4e93d84b7831676361e1160ed08657bdd6cf9d3
ivenkatraj <venkatraj442@gmail.com>
2024-12-13 13:04:26 +0530

    Object practice

--- 10-objects/README.md
+++ 10-objects/README.md
@@ -1,3 +1,8 @@
 # Objects
 
-Objects are 
+Objects are used to store more data types. That type is called an object.<br/>
+> Eg: var obj={
+    name: "John",
+    age: 20,
+    male: true
+}
---
+++ 10-objects/practice/script.js
@@ -0,0 +1,14 @@
+let object ={
+    person: "John",
+    age : 20,
+    gender: 'male',
+    dob: '24-04-2002',
+    nickname: [1,2,3]
+}
+
+let array = [1,2,3];
+
+console.log(typeof array);
+
+//using dot notation to call the value of a particular key
+console.log(typeof object.nickname);
