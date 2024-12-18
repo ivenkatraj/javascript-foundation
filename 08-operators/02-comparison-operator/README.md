06f7acbef684d9238dd2f97d67d904cfb43f9653
ivenkatraj <venkatraj442@gmail.com>
2024-12-18 22:01:17 +0530

    Comparison operator

---
+++ 08-operators/02-comparison-operator/README.md
@@ -0,0 +1,5 @@
+# Comparison Operator
+
+Comparison operator compare two values and return boolean values.
+
+The symbols are = > ! operators.
---
+++ 08-operators/02-comparison-operator/js/script.js
@@ -0,0 +1,16 @@
+const a = 5, b =10;
+
+//Greater than symbol
+console.log(a > b);
+
+//Less than symbol
+console.log(a < b);
+
+//Greater than or equal to symbol
+console.log(a >= b);
+
+//Less than or equal to symbol
+console.log(a <= b);
+
+//Not equal
+console.log(a != b);
