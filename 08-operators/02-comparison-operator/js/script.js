0d2db1be7e22484a954a1cdb5dcb3f7997cf2b72
ivenkatraj <venkatraj442@gmail.com>
2024-12-18 22:22:12 +0530

    Comparison operator

--- 08-operators/02-comparison-operator/js/script.js
+++ 08-operators/02-comparison-operator/js/script.js
@@ -1,16 +1,27 @@
-const a = 5, b =10;
+const a = 5, b = 10;
+
+console.log(`The given values are a=${a} & b=${b}`);
 
 //Greater than symbol
-console.log(a > b);
+console.log("a greater than b", a > b);
 
 //Less than symbol
-console.log(a < b);
+console.log("a less than b", a < b);
 
 //Greater than or equal to symbol
-console.log(a >= b);
+console.log("a greater than or equal to b", a >= b);
 
 //Less than or equal to symbol
-console.log(a <= b);
+console.log("a less than or equal to b", a <= b);
+
+//Is equal
+console.log("a is equal to b", a == b);
 
 //Not equal
-console.log(a != b);
+console.log("a not equal b", a != b);
+
+//Strict equality
+console.log("Strict equality: a===b", a === b);
+
+//Strict Inequality
+console.log(a !== b);
