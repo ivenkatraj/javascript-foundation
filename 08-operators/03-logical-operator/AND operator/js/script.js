06955accd3005ebce44722e9e246e526bde02916
ivenkatraj <venkatraj442@gmail.com>
2024-12-23 23:39:17 +0530

    Logical operators

---
+++ 08-operators/03-logical-operator/AND operator/js/script.js
@@ -0,0 +1,8 @@
+const a = 5, b =10;
+
+if (a === 5 && b ===10){
+    console.log("The condition is true");
+}
+else{
+    console.log("The condition is false");
+}
---
+++ 08-operators/03-logical-operator/NOT operator/js/script.js
@@ -0,0 +1,8 @@
+const a = 5, b =10;
+
+if (a === 51 || b ===100){
+    console.log("Atleast one condition is true");
+}
+else{
+    console.log("No condition is true");
+}
