e0077540059c7eb2cbe70e666aae10c5e360a841
ivenkatraj <venkatraj442@gmail.com>
2024-12-26 00:18:13 +0530

    Practice

---
+++ 08-operators/03-logical-operator/NOT operator/practice/script.js
@@ -0,0 +1,9 @@
+const age=20, name="John";
+
+if (age!=21 && name!="Sam"){
+    console.log("The data are true");
+}
+
+else{
+    console.log("The given data is false");
+}
---
+++ 08-operators/03-logical-operator/OR operator/practice/script.js
@@ -0,0 +1,9 @@
+const age = 20, name = "John";
+
+if (age === 20 || name === "Sam"){
+    console.log("Atleast one data is true");
+}
+
+else{
+    console.log("Neither the datas are true");
+}
