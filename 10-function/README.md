b5a759b0b7ef3ee2ee2c7dbf4006d798bbf4a30e
ivenkatraj <venkatraj442@gmail.com>
2025-03-20 20:44:27 +0530

    *function* intro

---
+++ 10-function/README.md
@@ -0,0 +1,7 @@
+# Function
+
+Function is a block of code that executes upon calling the function.
+
+There are two things to remember in function
+1. Function declaration
+2. Function call
---
+++ 10-function/js/script.js
@@ -0,0 +1,11 @@
+//BLOCK OF CODE... THE EXECUTES UPON CALLING
+
+// function declaration
+function square(number){ //the *number* is a parameter
+    return number * number;
+}
+
+// function call
+var result = square(5); // the *5* is the argument for the parameter
+
+console.log(result);
